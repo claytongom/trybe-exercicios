85673518302031c9a751fcb88600a9b45c11a74f
claytongom <claytoncnx@gmail.com>
2022-09-22 11:08:45 -0300

    resolução 'ex4' if/else - nota escolar

--- fundamentos/seção-4-introdução-a-JavaScript/dia-1-JS_primeiros-passos/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-1-JS_primeiros-passos/script.js
@@ -93,6 +93,35 @@ switch (peçaXadrez.toLowerCase()) {
     break;
 };
 
+// exercicio if and else nota escolar
+
+const grade = 90
+
+if(grade < 0 || grade > 100) {
+    console.log("Nota incorreta");
+} else if (grade >=90) {
+    console.log("A");
+} else if (grade >=80) {
+    console.log("B");
+} else if (grade >= 70) {
+    console.log("C");
+} else if (grade >= 60) {
+    console.log("D");
+} else if (grade >= 50) {
+    console.log("E");
+} else {
+   console.log("F");
+}
+
+
+
+}
+
+}
+
+
+
+
 
 
 
