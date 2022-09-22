a1ae847578fe9e998af916e56286e2c0112cfa52
claytongom <claytoncnx@gmail.com>
2022-09-22 10:35:20 -0300

    resolução ex2 - if/else

--- fundamentos/seção-4-introdução-a-JavaScript/dia-1-JS_primeiros-passos/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-1-JS_primeiros-passos/script.js
@@ -7,3 +7,64 @@ console.log("Multiplicação:" + (a * b));
 console.log("Divisão:" + (a / b));
 console.log("Módulo:" + (a % b));
 
+const value1 = 7
+const value2 = 10
+
+if (value1 > value2){
+    console.log(" value1 é maior que value2");
+} else {
+    console.log(" value2 é maior que value1");
+}
+
+const value1 = 7
+const value2 = 10
+const value3 = 3
+
+if (value1 > value2 && value1 > value3) {
+    console.log('O maior número é: ' + value1 + ' (value1)');
+  } else if (value2 > value1 && value2 > value3) {
+    console.log('O maior número é: ' + value2 + ' (value2)');
+  } else {
+    console.log('O maior número é: ' + value3 + ' (value3)');
+  };
+
+  
+const value = 5
+
+if (value > 0) {
+    console.log ('positivo');
+}
+else if (value < 0) {
+    console.log('negativo')
+}
+else {
+    console.log('zero')
+}
+
+
+const angloA= -40;
+const angloB = 90;
+const angloC = 25;
+
+let somaDosAngulos = angloA + angloB + angloC;
+
+let anglosPositivos = angloA > 0 && angloB > 0 && angloC > 0;
+
+if(anglosPositivos){
+  if (somaDosAngulos === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  };
+} else {
+  console.log('Erro: ângulo inválido');
+}
+
+
+
+
+
+
+
+
+
