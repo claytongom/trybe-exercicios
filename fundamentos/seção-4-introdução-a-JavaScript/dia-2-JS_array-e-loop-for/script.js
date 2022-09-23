4d695b8aabb299a3e84f7cdca2e8c568cd1f1f49
claytongom <claytoncnx@gmail.com>
2022-09-23 17:40:17 -0300

    alterações

--- fundamentos/seção-4-introdução-a-JavaScript/dia-2-JS_array-e-loop-for/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-2-JS_array-e-loop-for/script.js
@@ -90,11 +90,21 @@ let numeros = [];
 
 // exercicio 9 - resultados index / 2
 
-  for (let index = 1; index <= 25; index += 1) {
-    numeros.push(index / 2);
-  }
+  //for (let index = 1; index <= 25; index += 1) {
+    //numeros.push(index / 2);
+  //}
   
-  console.log(numeros);
+  //console.log(numeros);
+
+
+  const n = '8' ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
+
 
 
 
