1938d3b38aa04cd993ae9c109d996f93dd5b1743
claytongom <claytoncnx@gmail.com>
2022-09-23 09:55:56 -0300

    resolução exercicio 2

--- fundamentos/seção-4-introdução-a-JavaScript/dia-2-JS_array-e-loop-for/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-2-JS_array-e-loop-for/script.js
@@ -2,7 +2,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio 1 - percorrendo index com console.log
 
-for(let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
-}
+//for(let index = 0; index < numbers.length; index += 1) {
+  //console.log(numbers[index]);
+//}
 
+// exercicio 2 - Soma todos os valores contidos no array
+
+let resultado = 0;
+for(let index = 0; index < numbers.length; index += 1) {
+    resultado += numbers[index];
+}
+console.log(resultado);
