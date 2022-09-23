eb3b7483503efb66ef9a9f5295fde27748e6220b
claytongom <claytoncnx@gmail.com>
2022-09-23 10:45:56 -0300

    resolução exercicio 5

--- fundamentos/seção-4-introdução-a-JavaScript/dia-2-JS_array-e-loop-for/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-2-JS_array-e-loop-for/script.js
@@ -16,23 +16,38 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio 3 - Media do index
 
-let soma = 0;
+//let soma = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  soma += numbers[index];
-}
+//for (let index = 0; index < numbers.length; index += 1) {
+  //soma += numbers[index];
+//}
 
-let media = soma / numbers.length;
+//let media = soma / numbers.length;
 
-console.log(media);
+//console.log(media);
 
 // exercicio 4 - imprima a mensagem: “valor maior que 20” e  “valor menor ou igual a 20”
 
-if(media > 20) {
-    console.log("valor maior que 20")
-} else {
-    console.log("valor menor ou igual a 20")
+//if(media > 20) {
+    //console.log("valor maior que 20")
+//} else {
+    //console.log("valor menor ou igual a 20")
+//}
+
+// exercicio 5 -  maior valor contido no array e imprima-o
+
+
+let maiorNumero = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > maiorNumero) {
+    maiorNumero = numbers[index];
+  }
 }
+
+console.log(maiorNumero);
+
+
 
 
 
