c7732eeb0123378f77af4dd3929af3bae2717204
claytongom <claytoncnx@gmail.com>
2022-09-26 19:29:46 -0300

    resolução exercicio bonus 4

--- fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
@@ -87,23 +87,49 @@
 
 // bonus 3  - triangulo de asterisco (base 5 *) invertido
 
+//let n = 5;
+//let symbol = '*';
+//let inputLine = '';
+//let inputPosition = n - 1;
+
+//for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+ // for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+   // if (columnIndex < inputPosition) {
+   //   inputLine = inputLine + ' ';
+    //} else {
+     // inputLine = inputLine + symbol;
+   // }
+  //}
+  //console.log(inputLine);
+  //inputLine = '';
+  //inputPosition -= 1;
+//};
+
+
+// bonus 4  - piramide de asteriscos com n base
+
 let n = 5;
 let symbol = '*';
 let inputLine = '';
-let inputPosition = n - 1;
 
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
-    if (columnIndex < inputPosition) {
-      inputLine = inputLine + ' ';
-    } else {
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
       inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
     }
   }
   console.log(inputLine);
   inputLine = '';
-  inputPosition -= 1;
+  controlRight += 1;
+  controlLeft -= 1
 };
+
 
 
 
