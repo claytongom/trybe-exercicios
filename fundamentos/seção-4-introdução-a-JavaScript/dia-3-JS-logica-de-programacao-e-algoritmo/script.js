3c32937dc1a1c550ffa73702be1bedc77fe8f70c
claytongom <claytoncnx@gmail.com>
2022-09-26 19:47:59 -0300

    resolução exercicio bonus 5

--- fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
@@ -108,28 +108,48 @@
 
 // bonus 4  - piramide de asteriscos com n base
 
-let n = 5;
+//let n = 5;
+//let symbol = '*';
+//let inputLine = '';
+
+//let midOfMatrix = (n + 1) / 2;
+//let controlLeft = midOfMatrix;
+//let controlRight = midOfMatrix;
+
+//for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  //for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+   // if (columnIndex > controlLeft && columnIndex < controlRight) {
+     // inputLine = inputLine + symbol;
+    //} else {
+     // inputLine = inputLine + ' ';
+   // }
+  //}
+  //console.log(inputLine);
+  //inputLine = '';
+  //controlRight += 1;
+  //controlLeft -= 1
+//};
+
+//exercicio 5 - pirâmide com n asteriscos de base que seja vazia no meio. 
+//Assuma que o valor de n será sempre ímpar:
+
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
 let symbol = '*';
-let inputLine = '';
-
-let midOfMatrix = (n + 1) / 2;
-let controlLeft = midOfMatrix;
-let controlRight = midOfMatrix;
-
-for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex > controlLeft && columnIndex < controlRight) {
-      inputLine = inputLine + symbol;
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
     } else {
-      inputLine = inputLine + ' ';
+      outputLine += ' ';
     }
   }
-  console.log(inputLine);
-  inputLine = '';
+  controlLeft -= 1;
   controlRight += 1;
-  controlLeft -= 1
-};
-
-
+  console.log(outputLine);
+}
 
 
