790dbbcbe3acc45e927d0273508354ed0f2e3170
claytongom <claytoncnx@gmail.com>
2022-09-26 18:46:00 -0300

    resolução exercicio 2

--- fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
@@ -1,10 +1,23 @@
 // exercicio 1
 //crie um algoritmo que imprima na tela o fatorial de 10
 
-let fatorial = 1;
+//let fatorial = 1;
 
-for (let index = 10; index > 0; index -= 1) {
-  fatorial *= index;
+//for (let index = 10; index > 0; index -= 1) {
+  //fatorial *= index;
+//}
+
+//console.log(fatorial);
+
+// exercicio 2 - inverter uma palavra
+
+let word = 'tryber';
+
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
 }
 
-console.log(fatorial);
+console.log(reverseWord);
+
