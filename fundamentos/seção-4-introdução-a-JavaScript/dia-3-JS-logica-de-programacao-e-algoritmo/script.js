7174fa16a5e8e54b363f8f63bef6fa4a644c689f
claytongom <claytoncnx@gmail.com>
2022-09-26 19:20:20 -0300

    resolução exercicio bonus 3

--- fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
@@ -76,12 +76,34 @@
 
 // bonus 2  - triangulo de asterisco (base 5 *)
 
-let size = 5;
+//let size = 5;
+//let symbol = '*';
+//let inputLine = '';
+
+//for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  //console.log(inputLine);
+  //inputLine = inputLine + symbol;
+//};
+
+// bonus 3  - triangulo de asterisco (base 5 *) invertido
+
+let n = 5;
 let symbol = '*';
 let inputLine = '';
+let inputPosition = n - 1;
 
-for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
   console.log(inputLine);
-  inputLine = inputLine + symbol;
+  inputLine = '';
+  inputPosition -= 1;
 };
+
+
 
