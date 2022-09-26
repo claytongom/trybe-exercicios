ddcbdd1df8f1d2f86925ecb6e6dce2b0c04ebc14
claytongom <claytoncnx@gmail.com>
2022-09-26 18:52:51 -0300

    resolução exercicio 3

--- fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
@@ -11,13 +11,34 @@
 
 // exercicio 2 - inverter uma palavra
 
-let word = 'tryber';
+//let word = 'tryber';
 
-let reverseWord = '';
+//let reverseWord = '';
 
-for (let index = 0; index < word.length; index += 1) {
-  reverseWord += word[word.length - 1 - index];
+//for (let index = 0; index < word.length; index += 1) {
+  //reverseWord += word[word.length - 1 - index];
+//}
+
+//console.log(reverseWord);
+
+// exercicio - 3 maior palavra do array
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
 }
 
-console.log(reverseWord);
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
 
+console.log(biggestWord);
+console.log(smallestWord);
