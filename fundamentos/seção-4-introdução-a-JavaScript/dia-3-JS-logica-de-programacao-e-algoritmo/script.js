913450c7f96b20a4e4e0b50b1444e4bb8a59d9a1
claytongom <claytoncnx@gmail.com>
2022-09-26 18:59:12 -0300

    resolução exercicio 4

--- fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/seção-4-introdução-a-JavaScript/dia-3-JS-logica-de-programacao-e-algoritmo/script.js
@@ -23,22 +23,40 @@
 
 // exercicio - 3 maior palavra do array
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+//let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-let biggestWord = array[0];
-let smallestWord = array[0];
+//let biggestWord = array[0];
+//let smallestWord = array[0];
 
-for (let index = 0; index < array.length; index += 1) {
-  if (array[index].length > biggestWord.length) {
-    biggestWord = array[index];
+//for (let index = 0; index < array.length; index += 1) {
+  //if (array[index].length > biggestWord.length) {
+    //biggestWord = array[index];
+ // }
+//}
+
+//for (let index = 0; index < array.length; index += 1) {
+ // if (array[index].length < smallestWord.length) {
+    //smallestWord = array[index];
+ // }
+//}
+
+//console.log(biggestWord);
+//console.log(smallestWord);
+
+// exercicio - 4 
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
   }
 }
 
-for (let index = 0; index < array.length; index += 1) {
-  if (array[index].length < smallestWord.length) {
-    smallestWord = array[index];
-  }
-}
-
-console.log(biggestWord);
-console.log(smallestWord);
+console.log(biggestPrimeNumber);
