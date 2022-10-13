f4f0388709dbaababbaab20c4e8b689e6cd0dec3
claytongom <claytoncnx@gmail.com>
2022-10-13 09:44:21 -0300

    função limpar form com javascript

--- fundamentos/secao-6-html-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/secao-6-html-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,21 @@
-let submit = document.getElementById('submit-button');
+window.onload = function () {
+    const submitButton = document.querySelector('#submit-btn');
+    submitButton.addEventListener('click', );
+  };
 
-submit.addEventListener('click', function(event){
-event.preventDefault();
-})
+
+  function clearFields() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+  }
+
+  window.onload = function () {
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearFields);
+     };
