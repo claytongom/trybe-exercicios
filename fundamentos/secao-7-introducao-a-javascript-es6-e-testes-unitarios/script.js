07a962b36be9008725968d8f28dd83114b8b8218
claytongom <claytoncnx@gmail.com>
2022-10-19 10:46:49 -0300

    passo 4 - Retire o console.log que está fora do if/else para rodar sem erro

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -1,13 +1,12 @@
-  const testingScope = (escopo) => { // Alterando estrutura para utilizar arrow function
+const testingScope = (escopo) => { // Alterando estrutura para utilizar arrow function
     if (escopo === true) {
         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)'; // Alterando o tipo da variável para const
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo!`; // Substituição das concatenações de strings para template literals
-      console.log(ifScope);
+        ifScope = `${ifScope} ótimo, fui utilizada no escopo!`; // Substituição das concatenações de strings para template literals
+        console.log(ifScope);
     } else {
         const elseScope = 'Não devo ser utilizada fora do meu escopo (else)'; // Alterando o tipo da variável para const
-      console.log(elseScope);
+        console.log(elseScope);
     }
-    console.log(`${ifScope} o que estou fazendo aqui? :O`); // Substituição das concatenações de strings para template literals
-  };
+};
 
-  testingScope(true);
+testingScope(true);
