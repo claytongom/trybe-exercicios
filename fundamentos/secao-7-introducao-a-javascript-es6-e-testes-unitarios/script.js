f7c2b2663184193d273edfe54cfa954bacc4a436
claytongom <claytoncnx@gmail.com>
2022-10-19 11:13:02 -0300

    ordenando lista dentro de um array

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -16,7 +16,16 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortOddsAndEvens = () => {
+    oddsAndEvens[0] = 2
+    oddsAndEvens[1] = 3
+    oddsAndEvens[2] = 4
+    oddsAndEvens[3] = 7
+    oddsAndEvens[4] = 10
+    oddsAndEvens[5] = 13
 
-// Seu código aqui.
+    return oddsAndEvens
+}
+const sortedArray = sortOddsAndEvens()
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+console.log(oddsAndEvens);
