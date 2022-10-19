4eda1e66ca09b17bde01b3b093890254acae791a
claytongom <claytoncnx@gmail.com>
2022-10-19 11:03:45 -0300

    add codigo exericio 2

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -1,12 +1,22 @@
-const testingScope = (escopo) => { // Alterando estrutura para utilizar arrow function
-    if (escopo === true) {
-        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)'; // Alterando o tipo da variável para const
-        ifScope = `${ifScope} ótimo, fui utilizada no escopo!`; // Substituição das concatenações de strings para template literals
-        console.log(ifScope);
-    } else {
-        const elseScope = 'Não devo ser utilizada fora do meu escopo (else)'; // Alterando o tipo da variável para const
-        console.log(elseScope);
-    }
-};
+// const testingScope = (escopo) => { // Alterando estrutura para utilizar arrow function
+//     if (escopo === true) {
+//         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)'; // Alterando o tipo da variável para const
+//         ifScope = `${ifScope} ótimo, fui utilizada no escopo!`; // Substituição das concatenações de strings para template literals
+//         console.log(ifScope);
+//     } else {
+//         const elseScope = 'Não devo ser utilizada fora do meu escopo (else)'; // Alterando o tipo da variável para const
+//         console.log(elseScope);
+//     }
+// };
 
-testingScope(true);
+// testingScope(true);
+
+// exercicio 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
