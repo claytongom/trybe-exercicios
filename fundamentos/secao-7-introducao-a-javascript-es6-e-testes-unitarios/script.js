1937f33314a02c7883fc59d73296e8d8ca27870a
claytongom <claytoncnx@gmail.com>
2022-10-19 10:43:01 -0300

    passo 3 - Alterando o tipo da variável para let e const

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -1,10 +1,10 @@
   const testingScope = (escopo) => { // Alterando estrutura para utilizar arrow function
     if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)'; // Alterando o tipo da variável para const
       ifScope = `${ifScope} ótimo, fui utilizada no escopo!`; // Substituição das concatenações de strings para template literals
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+        const elseScope = 'Não devo ser utilizada fora do meu escopo (else)'; // Alterando o tipo da variável para const
       console.log(elseScope);
     }
     console.log(`${ifScope} o que estou fazendo aqui? :O`); // Substituição das concatenações de strings para template literals
