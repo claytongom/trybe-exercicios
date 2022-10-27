12a93ca0909a108bf720a9d302ead844ce0728e5
claytongom <claytoncnx@gmail.com>
2022-10-27 12:18:57 -0300

    ex 1 - usando find

--- fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-2-javascript-es6-hof-foreach-find-somevery-sort-aberto-intem-de-menu-com-subitens/exercise1.js
+++ fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-2-javascript-es6-hof-foreach-find-somevery-sort-aberto-intem-de-menu-com-subitens/exercise1.js
@@ -62,3 +62,9 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  // Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+  function authorBornIn1947() {
+    return books.find((book) => book.author.birthYear === 1947).author.name;
+  };
+
+    
