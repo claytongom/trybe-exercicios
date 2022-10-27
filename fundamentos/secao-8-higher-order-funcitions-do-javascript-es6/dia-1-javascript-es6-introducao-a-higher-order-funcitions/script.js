f746e6e74fb5e403e38a19e1f8b2efaa3fea4a2a
claytongom <claytoncnx@gmail.com>
2022-10-27 09:57:53 -0300

    ex 1 - insere função com parametro nome da pessoa

--- fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-funcitions/script.js
+++ fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-funcitions/script.js
@@ -1,11 +1,18 @@
 // exericio 1
 const newEmployees = () => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: employeeGenerator('Pedro Gerra'),
+      // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: employeeGenerator('Luiza Drummond'),
+      // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: employeeGenerator('Carla Paiva'),
+      // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  
+
+
 
   
