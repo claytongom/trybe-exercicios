7b646589660ff36f7c1f4d605fc5312a759b9a55
claytongom <claytoncnx@gmail.com>
2022-10-27 19:40:04 -0300

    ex3- add varialve e forEach para percorrer todos os nomes no array names

---
+++ fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-4-javascript-es6-hof-redece/exercise3.js
@@ -0,0 +1,14 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  const expectedResult = 20;
+  
+  function containsA() {
+    // escreva seu código aqui
+    let totalOfLettersA = 0;
+    names.forEach((name) => {
+    });
+  }
