2a399dc99669247c0d184d327f1b903b7baf69b2
claytongom <claytoncnx@gmail.com>
2022-10-27 19:45:59 -0300

    ex3- add function split para receber string convertida em cacacteres

--- fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-4-javascript-es6-hof-redece/exercise3.js
+++ fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-4-javascript-es6-hof-redece/exercise3.js
@@ -2,13 +2,14 @@ const names = [
     'Aanemarie', 'Adervandes', 'Akifusa',
     'Abegildo', 'Adicellia', 'Aladonata',
     'Abeladerco', 'Adieidy', 'Alarucha',
-  ];
-  
-  const expectedResult = 20;
-  
-  function containsA() {
+];
+
+const expectedResult = 20;
+
+function containsA() {
     // escreva seu código aqui
     let totalOfLettersA = 0;
     names.forEach((name) => {
+        const lettersFromName = name.split('');
     });
-  }
+}
