436a9cd47e8085dd3e79e5a60b89d64e6d0d247f
claytongom <claytoncnx@gmail.com>
2022-10-27 10:27:56 -0300

    ex 2 - retorna callback com aposta e sorteado com string de msg

--- fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-funcitions/script.js
+++ fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-funcitions/script.js
@@ -18,11 +18,12 @@
 //   console.log(newEmployees(employeeGenerator));
 
 const numberChecker = (myNumber, number) => myNumber === number;
-const lotteryResult = () => {
+const lotteryResult = (myNumber, callback) => {
   const number = Math.floor((Math.random() * 5) + 1);
+    return callback(myNumber, number)? 'Lucky day, you won!' : 'Try Again!';
 };
 
-
+console.log(lotteryResult(2, numberChecker));
 
   
 
