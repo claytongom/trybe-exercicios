b31d90c5721c0242bb2707abc7c6784623c5d60b
claytongom <claytoncnx@gmail.com>
2022-10-27 10:19:36 -0300

    ex 2 - add callback que verifica se numeros são iguais - aposta/sorteado

--- fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-funcitions/script.js
+++ fundamentos/secao-8-higher-order-funcitions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-funcitions/script.js
@@ -1,21 +1,24 @@
-// exericio 1
-const newEmployees = () => {
-    const employees = {
-      id1: employeeGenerator('Pedro Gerra'),
-      // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: employeeGenerator('Luiza Drummond'),
-      // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: employeeGenerator('Carla Paiva'),
-      // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
+// // exericio 1
+// const newEmployees = () => {
+//     const employees = {
+//       id1: employeeGenerator('Pedro Gerra'),
+//       // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//       id2: employeeGenerator('Luiza Drummond'),
+//       // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//       id3: employeeGenerator('Carla Paiva'),
+//       // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//     }
+//     return employees;
+//   };
 
-  const employeeGenerator = (fullName) => {
-    const email = fullName.toLowerCase().split(' ').join('_');
-    return {fullName, email: `${email}@betrybe.com`};
-  }
-  console.log(newEmployees(employeeGenerator));
+//   const employeeGenerator = (fullName) => {
+//     const email = fullName.toLowerCase().split(' ').join('_');
+//     return {fullName, email: `${email}@betrybe.com`};
+//   }
+//   console.log(newEmployees(employeeGenerator));
+
+const numberChecker = (myNumber, number) => myNumber === number;
+
 
 
   
