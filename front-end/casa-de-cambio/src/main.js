28893053237880b65ace785d3252598a2e640322
claytongom <claytoncnx@gmail.com>
2022-11-14 12:02:40 -0300

    adicionado API com function fetchAPI

--- front-end/casa-de-cambio/src/main.js
+++ front-end/casa-de-cambio/src/main.js
@@ -1,6 +1,15 @@
 const searchButton = document.querySelector(".search-btn");
+
+function fetchAPI(coin){
+    const url = `https://api.exchangerate.host/lates?base=${coin}`;
+    return fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+
+}
+
 function handleSearch(){
     console.log("clicou");
 }
 
-searchButton.addEventListener('click', handleSearch);
+searchButton.addEventListener('click', () => fetchAPI('USD'));
