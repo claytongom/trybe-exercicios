4072b547676ccc7335b55461cae3c7d15fea59d0
claytongom <claytoncnx@gmail.com>
2022-11-14 13:09:00 -0300

    imprimindo dados coinName e value em formato de array

--- front-end/casa-de-cambio/src/main.js
+++ front-end/casa-de-cambio/src/main.js
@@ -2,18 +2,34 @@ const searchButton = document.querySelector(".search-btn");
 
 const coinInput = document.querySelector("#coin-input")
 
+const coinsList = document.querySelector(".coins")
+
 function fetchAPI(coin){
     const url = `https://api.exchangerate.host/lates?base=${coin}`;
     return fetch(url)
     .then((response) => response.json())
     .then((data) => data.rates)
+}
 
+function renderCoins(coins){
+    coinsList.innerHTML = '';
+
+    const coinsArray = Object.entries(coins)
+    console.log(coinsArray)
+
+    coinsArray.forEach((coin) => {
+        const [coinName, value] = coin;
+        const li = document.createElement('li');
+        li.textContent = `${coinName} - ${value}`;
+        coinsList.appendChild(li);
+        
+    })
 }
 
 function handleSearch(){
     const coin = coinInput.value;
     fetchAPI(coin)
-    .then(console.log)
+    .then(renderCoins)
 }
 
 searchButton.addEventListener('click', handleSearch);
